69850fff871277c07555916ced4a2c16755bf5e7
Floris Romeijn <fromeijn@rocsys.com>
2024-12-16 19:43:07 +0100

    feat: add clock winder task

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use chrono::{Local, NaiveDateTime, Timelike};
-use log::*;
-use rppal::gpio::{Gpio, InputPin, Level};
+use core::fmt;
+use log::info;
+use rppal::gpio::{Gpio, InputPin, Level, OutputPin};
 use serde::{Deserialize, Serialize};
 use std::path::Path;
 use std::sync::mpsc;
@@ -24,6 +25,20 @@ struct MqttAppStatus {
 struct MqttClockTime {
     number_of_chimes: u8,
     offset_seconds: f32,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum ClockWinderStatus {
+    Unknown,
+    Idle,
+    WindingTimekeeping,
+    WindingStriking,
+}
+
+impl fmt::Display for ClockWinderStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
 
 fn read_config<P: AsRef<Path>>(path: P) -> Config {
@@ -70,17 +85,41 @@ fn main() {
     .expect("Unable to publish app status to mqtt broker");
 
     let gpio = Gpio::new().expect("Unable to get raspberry pi GPIOs");
-    let chime_lever_pin: InputPin = gpio
+    let chime_lever_pin = gpio
         .get(16)
         .expect("Unable to get chime lever input")
         .into_input_pullup();
     let (time_of_clock_tx, time_of_clock_rx) = mpsc::channel();
+    let clock_winder_motor_timekeeping_direction = gpio
+        .get(1)
+        .expect("Unable to get clock winder motor for timekeeping direction pin")
+        .into_output_low();
+    let clock_winder_motor_striking_direction = gpio
+        .get(2)
+        .expect("Unable to get clock winder motor for striking direction pin")
+        .into_output_low();
+    let clock_winder_timekeeping_request = gpio
+        .get(3)
+        .expect("Unable to get timekeeping winding request pin")
+        .into_input();
+    let clock_winder_striking_request = gpio
+        .get(4)
+        .expect("Unable to get striking winding request pin")
+        .into_input();
+    let (clock_winder_status_tx, clock_winder_status_rx) = mpsc::channel();
+
+    clock_winder(
+        clock_winder_motor_timekeeping_direction,
+        clock_winder_motor_striking_direction,
+        clock_winder_timekeeping_request,
+        clock_winder_striking_request,
+        clock_winder_status_tx,
+    );
 
     monitor_time_of_clock(chime_lever_pin, time_of_clock_tx);
 
-
     loop {
-        if let Ok(clock_time) = time_of_clock_rx.recv() {
+        if let Ok(clock_time) = time_of_clock_rx.try_recv() {
             info!("Time of Clock: {:?}", clock_time);
 
             mqtt.publish(paho_mqtt::Message::new(
@@ -90,6 +129,19 @@ fn main() {
             ))
             .expect("Unable to publish clock time to mqtt broker");
         }
+
+        if let Ok(clock_winder_status) = clock_winder_status_rx.try_recv() {
+            info!("Clock winder status: {:?}", clock_winder_status);
+
+            mqtt.publish(paho_mqtt::Message::new(
+                "rust/ClockWinder",
+                clock_winder_status.to_string(),
+                0,
+            ))
+            .expect("Unable to publish clock time to mqtt broker");
+        }
+
+        thread::sleep(Duration::from_millis(100)); // Push data very 100 ms
     }
 }
 
@@ -135,6 +187,40 @@ fn monitor_time_of_clock(chime_lever_pin: InputPin, tx: mpsc::Sender<MqttClockTi
                 first_transition_time = None;
             }
 
+            thread::sleep(Duration::from_millis(100)); // Polling interval
+        }
+    });
+}
+
+fn clock_winder(
+    mut motor_timekeeping: OutputPin,
+    mut motor_striking: OutputPin,
+    request_timekeeping: InputPin,
+    request_striking: InputPin,
+    status: mpsc::Sender<ClockWinderStatus>,
+) {
+    thread::spawn(move || {
+        let mut last_status = ClockWinderStatus::Unknown;
+        let mut current_status = ClockWinderStatus::Unknown;
+        loop {
+            if request_striking.is_high() {
+                current_status = ClockWinderStatus::WindingStriking;
+                motor_striking.set_high();
+                motor_timekeeping.set_low();
+            } else if request_timekeeping.is_high() {
+                current_status = ClockWinderStatus::WindingTimekeeping;
+                motor_striking.set_low();
+                motor_timekeeping.set_high();
+            } else {
+                current_status = ClockWinderStatus::Idle;
+                motor_striking.set_low();
+                motor_timekeeping.set_low();
+            }
+
+            if current_status != last_status {
+                last_status = current_status;
+                status.send(current_status.clone()).unwrap();
+            }
             thread::sleep(Duration::from_millis(100)); // Polling interval
         }
     });
